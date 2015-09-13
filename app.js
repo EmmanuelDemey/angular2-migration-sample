e0e33c683c38be733a5ecc5e7ac3f7c593991325
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-09-13 18:42:57 +0200

    Missing Filter

--- app.js
+++ app.js
@@ -31,12 +31,13 @@ module.directive('beer', function(BrasserieService){
 				beer: '='
 			}, 
 			template: '<div class="col-md-4 panel panel-default">' +
-						'<div class="panel-heading">{{beer.name}}</div>' +
-							'<div class="panel-body">' +
-								'{{beer.description}}' +
-							'</div>' +
-							'<button ng-click="selectBeer()" class="btn btn-primary">Give me a pinte !</button>' +
-						'</div>',
+					'<div class="panel-heading">' +
+						'<span ng-class="{\'glyphicon glyphicon-heart\': {{beer.note | NoteFilter}} }"></span>{{beer.name}}</div>' +
+						'<div class="panel-body">' +
+							'{{beer.description}}' +
+						'</div>' +
+						'<button ng-click="selectBeer()" class="btn btn-primary">Give me a pinte !</button>' +
+					'</div>',
 			link: function($scope){
 				$scope.selectBeer = function(){
 					BrasserieService.getOnePinte($scope.beer);
